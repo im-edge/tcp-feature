48555e1ab8fbd60b93a2cea343086035cbc4d789
Thomas Gelf <thomas@gelf.net>
2024-07-13 22:20:05 +0200

    TcpApi: use Amp\Socket

--- src/TcpApi.php
+++ src/TcpApi.php
@@ -2,34 +2,32 @@
 
 namespace IMEdge\TcpFeature;
 
+use Amp\Cancellation;
+use Amp\DeferredCancellation;
+use Amp\Socket\ConnectContext;
 use Exception;
+use IMEdge\Protocol\NTP\Util;
 use IMEdge\RpcApi\ApiMethod;
 use IMEdge\RpcApi\ApiNamespace;
-use React\Dns\Model\Message;
-use React\Dns\Query\Query;
-use React\Dns\Query\TimeoutExecutor;
-use React\Dns\Query\UdpTransportExecutor;
-use React\Promise\Deferred;
-use React\Promise\PromiseInterface;
-use React\Socket\ConnectionInterface;
-use React\Socket\TcpConnector;
-use React\Socket\TimeoutConnector;
 
+use function Amp\async;
+use function Amp\Future\awaitAll;
+use function Amp\Socket\connect;
 use function implode;
-use function ip2long;
 use function ksort;
 use function hrtime;
-use function React\Async\await as reactAwait;
 
 #[ApiNamespace('tcp')]
 class TcpApi
 {
-    protected TcpConnector|TimeoutConnector $connector;
+    protected ConnectContext $connectContext;
+
+    /** @var Cancellation[] */
+    protected array $pendingCancellations = [];
 
     public function __construct()
     {
-        $connector = new TcpConnector();
-        $this->connector = new TimeoutConnector($connector, 5);
+        $this->connectContext = (new ConnectContext)->withConnectTimeout(5);
     }
 
     /**
@@ -39,121 +37,72 @@ class TcpApi
     #[ApiMethod]
     public function check(string $ip, int $port): array
     {
-        return reactAwait($this->checkAsync($ip, $port));
-    }
+        $cancellation = new DeferredCancellation();
+        $this->pendingCancellations[] = $cancellation;
+        $result = [
+            'reachable' => false,
+            'message'   => '',
+        ];
 
-    public function checkAsync(string $ip, int $port): PromiseInterface
-    {
-        if (self::isIp($ip)) {
-            return $this->checkTcp($ip, $port);
-        } else {
-            $deferred = new Deferred();
-
-            $this->resolveDomain($ip)->then(function ($ips) use ($port, $deferred) {
-                $pending = [];
-                $result = [
-                    'reachable' => false,
-                    'message'   => '',
-                ];
-                $messages = [];
-                // $ips[] = '8.8.8.6'; // -> Timeout test
-                // TODO: Align logic, single IP check differs from this one.
-                //       Should point to the same method, plus collection
-                foreach ($ips as $ip) {
-                    $handler = function ($res) use (&$result, &$messages, $ip, &$pending, $deferred) {
-                        // $result[$ip] = $res;
-                        if ($res instanceof Exception) {
-                            $messages[$ip] = $res->getMessage();
-                            // unset($pending[$ip]);
-                        } elseif (is_array($res)) {
-                            if ($res['reachable']) {
-                                $result['reachable'] = true;
-                            }
-                            $messages[$ip] = $res['message'];
-                            unset($pending[$ip]);
-                        } else {
-                            $deferred->reject('WHAT?');
-                        }
-                        if (empty($pending)) {
-                            ksort($messages);
-                            $result['message'] = implode(', ', $messages);
-                            $deferred->resolve($result);
-                        }
-                    };
-                    $pending[$ip] = $this->checkTcp($ip, $port)->then($handler)->otherwise($handler);
-                }
-            }, function (Exception $e) use ($deferred) {
-                $result = [
-                    'reachable' => false,
-                    'message'   => $e->getMessage(),
-                ];
-                $deferred->resolve($result);
-            });
-
-            return $deferred->promise();
-        }
-    }
-
-    protected function resolveDomain($domain)
-    {
-        $connector = new UdpTransportExecutor('9.9.9.9');
-        // $connector = new UdpTransportExecutor('172.31.15.1', $this->loop);
-        // $connector = new UdpTransportExecutor('192.168.64.9', $this->loop);
-        $resolver = new TimeoutExecutor(
-            $connector,
-            3,
-        );
-
-        return $resolver
-            ->query(new Query($domain, Message::TYPE_A, Message::CLASS_IN))
-            ->then(function (Message $message) {
-                $ips = [];
-                foreach ($message->answers as $answer) {
-                    if ($answer->type === Message::TYPE_A) { // There might be CNAMEs too
-                        $ips[] = $answer->data;
+        try {
+            $ips = Util::getIps($ip);
+            if (empty($ips)) {
+                throw new \InvalidArgumentException("Failed to resolve $ip");
+            }
+            $futures = [];
+            foreach ($ips as $ip) {
+                $futures[$ip] = async(function () use ($ip, $port, $cancellation) {
+                    $start = hrtime(true);
+                    try {
+                        $socket = connect("$ip:$port", $this->connectContext, $cancellation->getCancellation());
+                        $duration = hrtime(true) - $start;
+                        $socket->close();
+                        return [
+                            'reachable' => true,
+                            'message'   => sprintf(
+                                'Successfully connected to %s:%s via TCP in %.2Fms',
+                                $ip,
+                                $port,
+                                $duration * 1_000_000
+                            )
+                        ];
+                    } catch (Exception $e) {
+                        $duration = hrtime(true) - $start;
+                        return [
+                            'reachable' => false,
+                            'message' => sprintf(
+                                'Failed to connect to %s:%s after %.2Fms', // . $e->getMessage(),
+                                $ip,
+                                $port,
+                                $duration * 1_000_000
+                            )
+                        ];
                     }
+                });
+            }
+            [$errors, $results] = awaitAll($futures);
+            foreach ($errors as $error) {
+                throw $error; // There should be none
+            }
+            $messages = [];
+            foreach ($results as $ip => $singleResult) {
+                if ($singleResult['reachable']) {
+                    $result['reachable'] = true;
                 }
+            }
+            ksort($messages);
+            $result['message'] = implode(', ', $messages);
+        } catch (Exception $e) {
+            $result['message'] = $e->getMessage();
+        }
 
-                return $ips;
-            });
+        return $result;
     }
 
-    protected static function isIp($ip): bool
+    public function stop(): void
     {
-        return ip2long($ip) !== false;
-    }
-
-    protected function checkTcp($ip, $port)
-    {
-        // Logger::info("IP=$ip");
-        $start = hrtime(true);
-
-        $success = function (ConnectionInterface $connection) use ($ip, $port, $start) {
-            $duration = hrtime(true) - $start;
-            $connection->close();
-            return [
-                'reachable' => true,
-                'message'   => sprintf(
-                    'Successfully connected to %s:%s via TCP in %.2Fms',
-                    $ip,
-                    $port,
-                    $duration * 1_000_000
-                )
-            ];
-        };
-        $failure = function (Exception $e) use ($ip, $port, $start) {
-            $duration = hrtime(true) - $start;
-            return [
-                'reachable' => false,
-                'message' => sprintf(
-                    'Failed to connect to %s:%s after %.2Fms', // . $e->getMessage(),
-                    $ip,
-                    $port,
-                    $duration * 1_000_000
-                )
-            ];
-        };
-
-        return $this->connector->connect("tcp://$ip:$port")->then($success, $failure);
+        foreach ($this->pendingCancellations as $cancellation) {
+            $cancellation->throwIfRequested(); // TODO: verify this
+        }
     }
 }
